c239ce4055785fb0f96dcad8ac174d1d9bc33954
Galiullin Nikolai <galiullinnikolai@gmail.com>
2016-01-31 22:07:47 +0200

    екенне

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+    void OnTriggerEnter2D()
+    {
+        Application.LoadLevel("Mountains");
+    }
+
+}
