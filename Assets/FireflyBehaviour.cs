ae2f49cab521595ab2e4b9fb828f3f32fb090cb0
Galiullin Nikolai <galiullinnikolai@gmail.com>
2016-01-31 23:50:55 +0200

    Светлячки + веревка + ступни

---
+++ Assets/FireflyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflyBehaviour : MonoBehaviour {
+
+    public Vector2 maxdP;
+    public float depth;
+    Vector2 prevCameraPosition;
+    Vector2 curCameraPosition;
+    Vector2 dv;
+
+	void Start () 
+    {
+        depth = Random.RandomRange(0.1f, 3f);   
+	}
+	
+	void Update () 
+    {
+        Vector2 curCameraPosition = (Vector2)Camera.main.transform.position;
+	    if(prevCameraPosition != curCameraPosition)
+        {
+            dv = curCameraPosition - prevCameraPosition;
+        }
+        transform.position += (Vector3)(dv * depth);
+        dv = new Vector2(Random.RandomRange(-0.001f, 0.001f), Random.RandomRange(-0.001f, 0.001f));
+        transform.position += (Vector3)(dv * depth);
+        prevCameraPosition = curCameraPosition;
+	}
+}
---
+++ Assets/FireflyGeneration.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireflyGeneration : MonoBehaviour {
+
+    public int countOfFireflies = 10;
+    //GameObject[] ff;
+    public GameObject ffPrefab;
+
+	void Start ()
+    {
+        float minX = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x;
+        float minY = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).y;
+        float maxX = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).x;
+        float maxY = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).y;
+
+        for(int i = 0; i < countOfFireflies; i++)
+        {
+            Instantiate(ffPrefab, new Vector3(Random.RandomRange(minX, maxX), Random.RandomRange(minY, maxY), 0), transform.rotation);
+        }
+	}
+}
