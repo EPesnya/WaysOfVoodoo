a9f82324de00dc031a3d549b19f764624dcc8e9b
Galiullin Nikolai <galiullinnikolai@gmail.com>
2016-04-14 23:18:45 +0200

    Параллакс и рыбак
    -добавлен параллакс в горной сцене (фон порезан и находится в
    Scenes\Mounains)
    -рыбак (без нарисованного спрайта) заанимирован на ничегонеделание

---
+++ Assets/Paralax.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paralax : MonoBehaviour {
+
+    public float depth;
+    Vector2 AnchorPos;
+    Vector2 AnchorCamPos;
+    Vector2 prevCameraPosition;
+    Vector2 curCameraPosition;
+    Vector2 dv;
+
+	void Start () 
+    {
+        AnchorPos = transform.position;
+        AnchorCamPos = (Vector2)Camera.main.transform.position;
+        //depth = 1 / (float)(1 - depth);
+	}
+	
+	void Update ()
+    {
+        Vector2 curCameraPosition = (Vector2)Camera.main.transform.position;
+        if (prevCameraPosition != curCameraPosition)
+        {
+            dv = curCameraPosition - AnchorCamPos;
+        }
+        transform.position = new Vector2(AnchorPos.x + dv.x * depth, AnchorPos.y);
+        prevCameraPosition = curCameraPosition;
+        	
+	}
+}
