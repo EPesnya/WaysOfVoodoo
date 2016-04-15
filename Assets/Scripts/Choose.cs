340bb0056a476f50da395849a169fc0274bfd850
PesnyaEvgeniy <pesnyaevgeniy@gmail.com>
2016-04-15 21:08:29 +0300

    Странный глаз

--- Assets/Scripts/Choose.cs
+++ Assets/Scripts/Choose.cs
@@ -5,27 +5,40 @@ public class Choose : MonoBehaviour {
 
     GameObject Player;
     public GameObject Eye;
+    public float DistanceX = 1.2f;
+    public float DistanceY = 1.2f;
+    bool isTurningOff = false;
+    float startOfTurningOff = -1;
+    bool isWasHere = false;
+    Animator Terminator;
 
 	void Start () {
         Player = GameObject.FindGameObjectWithTag("Player");
+        Terminator = Eye.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Mathf.Abs(this.transform.position.x - Player.transform.position.x) < 1.2f)
+        if (Mathf.Abs(this.transform.position.x - Player.transform.position.x) < DistanceX
+            && Mathf.Abs(this.transform.position.y - Player.transform.position.y) < DistanceY)
         {
-            if (Mathf.Abs(this.transform.position.y - Player.transform.position.y) < 1.2f)
-            {
-                Eye.SetActive(true);
-            }
-            else
-            {
-                Eye.SetActive(false);
-            }
+            Eye.SetActive(true);
+            isWasHere = true;
         }
         else
         {
+            if(isWasHere)
+            {
+                startOfTurningOff = Time.time;
+                isTurningOff = true;
+                Terminator.SetTrigger("shoutDown");
+                isWasHere = false;
+            }
+        }
+        if(isTurningOff && Time.time - startOfTurningOff > 1)
+        {
             Eye.SetActive(false);
+            isTurningOff = false;
         }
 	}
 }
