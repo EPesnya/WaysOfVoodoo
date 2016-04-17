3bd36697ab76c30c6e934d3967a25d165b887ef6
Galiullin Nikolai <galiullinnikolai@gmail.com>
2016-04-17 13:43:21 +0200

    Фиксы лестницы

--- Assets/Scripts/ForStairs.cs
+++ Assets/Scripts/ForStairs.cs
@@ -10,19 +10,23 @@ public class ForStairs : MonoBehaviour {
         platform = this.transform.GetChild(0).gameObject;
     }
 
-
     void OnTriggerStay2D(Collider2D other)
     {
         
         if (other.tag == "Player")
         {
             if (!HeroControls.grounded)
-            other.GetComponent<Animator>().SetBool("OnStairs", true);
+            {
+                other.GetComponent<Animator>().SetBool("OnStairs", true);
+                other.GetComponent<Animator>().speed = 0;
+                other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            }
             if (Input.GetKey("w"))
             {
                 //HeroControls.canMove = false;
                 other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
                 platform.GetComponent<EdgeCollider2D>().isTrigger = true;
+                other.GetComponent<Animator>().speed = 1;
                 other.gameObject.transform.position = new Vector2(other.gameObject.transform.position.x, other.gameObject.transform.position.y + 0.05f);
             }
             if (Input.GetKey("s"))
@@ -30,6 +34,7 @@ public class ForStairs : MonoBehaviour {
                 //HeroControls.canMove = false;
                 other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
                 platform.GetComponent<EdgeCollider2D>().isTrigger = true;
+                other.GetComponent<Animator>().speed = 1;
                 other.gameObject.transform.position = new Vector2(other.gameObject.transform.position.x, other.gameObject.transform.position.y - 0.05f);
             }
         }
@@ -41,6 +46,7 @@ public class ForStairs : MonoBehaviour {
             platform.GetComponent<EdgeCollider2D>().isTrigger = false;
             other.GetComponent<Animator>().SetBool("OnStairs", false);
             other.gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;
+            other.GetComponent<Animator>().speed = 1;
         }
     }
 
