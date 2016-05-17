6571905a43892178c59e735a4489d4d80cf0d287
SanQri <galiullinnikolai@gmail.com>
2016-05-17 19:46:37 +0300

    Ну вот тут много
    ну вот ЧП для камеры
    ну вот картинка
    и мб еще что-то не запланированное
    не так и много

--- Assets/Characters/Персонаж/Скрипты/CameraFollow.cs
+++ Assets/Characters/Персонаж/Скрипты/CameraFollow.cs
@@ -5,6 +5,10 @@ public class CameraFollow : MonoBehaviour {
 
     public float speed = 5;
     public float verticalShift = 3;
+    public static bool isFollowing = true;
+    public static Vector2 TargetPos;
+    public float defaultScale;
+    public static float targetScale;
     GameObject Player;
 
 	void Start () 
@@ -14,6 +18,15 @@ public class CameraFollow : MonoBehaviour {
 	
 	void FixedUpdate () 
     {
-        GetComponent<Rigidbody2D>().velocity = (new Vector2(Player.transform.position.x, Player.transform.position.y + verticalShift) - (Vector2)transform.position) ;
+        if (isFollowing)
+        {
+            GetComponent<Rigidbody2D>().velocity = (new Vector2(Player.transform.position.x, Player.transform.position.y + verticalShift) - (Vector2)transform.position);
+            this.GetComponent<Camera>().orthographicSize += (defaultScale - this.GetComponent<Camera>().orthographicSize) * 0.01f;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = ((Vector2)CameraFollow.TargetPos - (Vector2)transform.position);
+            this.GetComponent<Camera>().orthographicSize += (CameraFollow.targetScale - this.GetComponent<Camera>().orthographicSize) * 0.01f;
+        }
 	}
 }
