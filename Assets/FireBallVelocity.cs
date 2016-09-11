c1d2dc27a4fde7f7f2478498f0711449a7fa8116
EPesnya <pesnyaevgeniy@gmail.com>
2016-09-11 18:46:43 +0300

    ФАЙРБОЛ

---
+++ Assets/FireBallVelocity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBallVelocity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().AddForce((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * 80);
+        }
+	
+	}
+}
